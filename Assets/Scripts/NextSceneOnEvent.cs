e1b4e45165b94bf3f2b5116e73bd1eed72f2fe34
EilayAbu <royaeilay@gmail.com>
2026-07-29 14:30:50 +0300

    updates

--- Assets/Scripts/NextSceneOnEvent.cs
+++ Assets/Scripts/NextSceneOnEvent.cs
@@ -1,11 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextSceneOnEvent : MonoBehaviour
 {
+    [SerializeField, Min(0f)] private float delayBeforeNextScene = 10f;
+
     // Hook this up to any UnityEvent (button, trigger, etc.)
     public void GoToNextScene()
     {
+        StartCoroutine(LoadNextSceneAfterDelay());
+    }
+
+    private IEnumerator LoadNextSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeNextScene);
+
         int next = SceneManager.GetActiveScene().buildIndex + 1;
         if (next < SceneManager.sceneCountInBuildSettings)
             SceneManager.LoadScene(next);
